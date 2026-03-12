76f14ad84f2a7a9c6d6543b26a080e26eaefbf85
AlonsoFavero <alonso.favero@gmail.com>
2026-03-12 12:25:57 -0300

    criei o ex011

---
+++ aula12/ex011.js
@@ -0,0 +1,13 @@
+var idade = 66;
+console.log(`você tem ${idade} anos`)
+if(idade == 16 || idade == 17 || idade > 65){
+    console.log('voto opcional')
+} else {
+    if(idade >= 18){
+        console.log('voto obrigatório')
+    }
+    if(idade < 16){
+        console.log('não vota')
+    }
+    
+}

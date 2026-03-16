7a697d486071197e653d62a37b607d8c9de2bda2
AlonsoFavero <alonso.favero@gmail.com>
2026-03-16 16:37:22 -0300

    finalizei o meu codigo js

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,22 @@
-alert("Olá!");
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        //bom dia!
+        img.src = 'foto manha .jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18){
+        //boa tarde!
+        img.src = 'fototarde.jpg'
+        document.body.style.background = '#e64b0ed2'
+    }
+    else{
+        //boa noite!
+        img.src = 'foto noite.jpg'
+        document.body.style.background = '#000033'
+    }
+}

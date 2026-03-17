964c6727010f9dc4bb35fa852f93dd3d8f7ba68d
AlonsoFavero <alonso.favero@gmail.com>
2026-03-17 15:55:12 -0300

    iniciando um novo exercicio

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,18 @@
-alert("Olá!");
+function contar(){
+   let ini = document.getElementById('txti')
+   let fim = document.getElementById('txtf')
+   let passo = document.getElementById('txtp')
+   let res = document.getElementById('res')
+   
+   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.legth == 0) {
+    window.alert('[ERRO] faltam dados!')
+   } else{
+    res.innerHTML = 'contando...'
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+   }
+
+   }
+
+

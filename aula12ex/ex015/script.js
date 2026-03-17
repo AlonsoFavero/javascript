4e05788c6fc62b0a614a6acf6cea4303b04f05fb
AlonsoFavero <alonso.favero@gmail.com>
2026-03-17 14:03:07 -0300

    adicionei imagens no meu exercicio

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,12 +9,43 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if(fsex[0].checked){
             genero = 'Homem'
+
+            if(idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'bebehomem.jpg')
+            } else if (idade >= 10 && idade < 21){
+                //Jovem
+                img.setAttribute('src', 'jovemhomem.jpg')
+            } else if(idade < 50){
+                //Adulto
+                img.setAttribute('src', 'adultohomem.jpg')
+            } else{
+                //Idoso
+                img.setAttribute('src', 'idoso.jpg')
+            }
         } else if (fsex[1].checked){
             genero = 'Mulher'
+
+            if(idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'bebemulher.jpg')
+            } else if (idade >= 10 && idade < 21){
+                //Jovem
+                img.setAttribute('src', 'jovemmulher.jpg')
+            } else if(idade < 50){
+                //Adulto
+                img.setAttribute('src', 'adultomulher.jpg')
+            } else{
+                //Idoso
+                img.setAttribute('src', 'idosa.jpg')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }

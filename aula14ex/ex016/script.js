0bac25fb763194fb4bcc79115715e957c368c2e2
AlonsoFavero <alonso.favero@gmail.com>
2026-03-18 14:55:57 -0300

    exercicio finalizado

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,12 +5,30 @@ function contar(){
    let res = document.getElementById('res')
    
    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.legth == 0) {
-    window.alert('[ERRO] faltam dados!')
+    //window.alert('[ERRO] faltam dados!')
+    res.innerHTML = 'impossivel contar!'
    } else{
-    res.innerHTML = 'contando...'
+    res.innerHTML = 'contando... <br>'
     let i = Number(ini.value)
     let f = Number(fim.value)
     let p = Number(passo.value)
+    if(p <= 0){
+      window.alert('passo invalido! considerando PASSO 1')
+      p=1
+    }
+   if (i < f) {
+ //contagem crescente
+    for(let c = i ; c <= f ; c +=p){
+       res.innerHTML += ` ${c}  \u{1F449}` 
+         }
+
+      } else{
+         //contagem regressiva
+         for(let c = i; c >= f; c-= p){
+             res.innerHTML += ` ${c} \u{1F449}`
+         }
+      }
+       res.innerHTML += `\u{1F3C1}`
    }
 
    }

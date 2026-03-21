378afb55072c964cbdb28e205f090ecc464eb5a3
AlonsoFavero <alonso.favero@gmail.com>
2026-03-21 13:17:42 -0300

    iniciei as minhas experiencias praticas

---
+++ pratica/pratica01.js
@@ -0,0 +1,10 @@
+let nome = "Alonso"
+console.log(nome)
+
+let idade = 20
+
+if (idade >= 18){
+    console.log ("maior de idade")
+} else {
+    console.log("menor de idade")
+}
---
+++ pratica/pratica03.js
@@ -0,0 +1,9 @@
+let nota
+
+nota = 6
+
+if (nota >= 7){
+    console.log("aprovado")
+} else if (nota < 7){
+    console.log("reprovado")
+}

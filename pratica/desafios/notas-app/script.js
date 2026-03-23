53ad52a9d9503c3644c85efc0c2429ec8ef86eb8
AlonsoFavero <alonso.favero@gmail.com>
2026-03-23 15:51:58 -0300

    estruturando o script

--- pratica/desafios/notas-app/script.js
+++ pratica/desafios/notas-app/script.js
@@ -1,7 +1,10 @@
 let container = document.getElementById("container-notas")
-let div = document.createElement("div")
+
+let notas = []
+
 
 function criarnota(){
+
 let nota = document.createElement("div")
 let texto = document.createElement("textarea")
 let botao = document.createElement("button")
@@ -16,4 +19,34 @@ botao.addEventListener("click", function(){
   nota.remove()
 })
 
+texto.addEventListener("input", function(){
+   
+notas = []
+
+let todostextos = document.querySelectorAll("textarea")
+
+todostextos.forEach(function(item){
+    notas.push(item.value)
+})
+
+    console.log(notas)
+
+    //garante o salvameto ds notas
+    localStorage.setItem("notas", JSON.stringify(notas))
+
+})
 }
+
+//carregar ao abrir
+let dados = JSON.parse(localStorage.getItem("notas"))
+
+ if(dados){
+        dados.forEach(function(item){
+
+            criarnota()
+
+            let textareas = document.querySelectorAll("textarea")
+            textareas[textareas.length - 1].value = item
+        })
+    }
+

2a1e947ab2823457763e1360cade010423d7cb22
AlonsoFavero <alonso.favero@gmail.com>
2026-03-24 16:11:06 -0300

    . map()

---
+++ pratica/exercicios praticos/pratica08.js
@@ -0,0 +1,7 @@
+let numeros = [1, 2, 3, 4,]
+
+let dobro = numeros.map(n => n * 2)
+let quadrado = numeros.map(n => n* n)
+
+console.log("numero ao quadrado:", quadrado)
+console.log("numero ao dobro:", dobro)

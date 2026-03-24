0eb196ab2f451acb4b0f768e72031c293afe80fe
AlonsoFavero <alonso.favero@gmail.com>
2026-03-24 13:54:51 -0300

    criei pratica04js

---
+++ pratica/exercicios praticos/pratica04.js
@@ -0,0 +1,18 @@
+let numero = 0
+
+console.log(numero)
+
+if(numero > 7){
+ console.log("numero positivo")
+} else if (numero === 0){
+    console.log("o numeor é zero")
+}
+else{
+    console.log("numero negativo")
+}
+
+if(numero % 2 === 0){
+    console.log("numero é par")
+}else{
+    console.log("numeor é impar")
+}

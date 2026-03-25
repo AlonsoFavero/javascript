c4a4b60f1f8a35e683b16abd7f92b88951a79444
AlonsoFavero <alonso.favero@gmail.com>
2026-03-25 14:24:28 -0300

    exercicio de filter + reduc

---
+++ pratica/exercicios praticos/pratica16.js
@@ -0,0 +1,12 @@
+let numeros = [2, 3, 4, 5]
+
+let pares = numeros.filter(n => n % 2 === 0)
+
+let resultado = pares.reduce((total,n) =>{
+    return total * n
+}, 1 )
+
+console.log(resultado)
+
+
+ 
